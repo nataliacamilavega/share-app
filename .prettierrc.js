228f8c4eaf0b1e844fdf150e34ca2462ed9e7d4b
Natalia Vega <nataliacamilav@gmail.com>
2024-09-08 18:23:08 -0300

    Update eslint

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 100,
+    trailingComma: 'all',
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true, // Usa comillas simples
+    bracketSpacing: false,
+    arrowParens: 'always',
+    endOfLine: 'auto',
+  }
+  
--- components/bottom-navigation/TabBarButton.tsx
+++ components/bottom-navigation/TabBarButton.tsx
@@ -7,24 +7,14 @@ const TabBarButton = (props: BottomTabBarButtonProps) => {
   return (
     <TouchableOpacity {...props}>
       <Text
-        className=" text-white "
-        // style={{
-        //   color: 'white',
-        // }}
+        className=" text-white " 
+
       >
-        Si
       </Text>
       <View
-        // style={{
-        //   width: '100%',
-        //   height: 6,
-        //   backgroundColor: props?.accessibilityState?.selected
-        //     ? Theme.tabBar.borderTopColor
-        //     : 'transparent',
-        // }}
+
         className=" bg-slate-600  border-2 border-red-500 "
       />
-
       {props.children}
     </TouchableOpacity>
   )
